ee2ee1de1fcc2ebe5af63f7954bde5a2c9c5209a
shrikantsharma <shrikants0421@gmail.com>
2026-01-18 19:12:32 +0530

    intial setup

---
+++ app/src/main/java/com/example/productdetection/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.productdetection
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.core.content.ContextCompat
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            val context = this
+            var hasPermission by remember { mutableStateOf(false) }
+
+            val permissionLauncher =
+                rememberLauncherForActivityResult(
+                    ActivityResultContracts.RequestPermission()
+                ) { granted ->
+                    hasPermission = granted
+                }
+            LaunchedEffect(Unit) {
+                hasPermission =
+                    ContextCompat.checkSelfPermission(
+                        context,
+                        Manifest.permission.CAMERA
+                    ) == PackageManager.PERMISSION_GRANTED
+
+                if (!hasPermission) {
+                    permissionLauncher.launch(Manifest.permission.CAMERA)
+                }
+            }
+            if (hasPermission) {
+
+            }
+        }
+    }
+}
+
